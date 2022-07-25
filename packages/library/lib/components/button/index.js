78dc827e40445845136aa4c365ad5cae226b9dd8
Conor Butler <conor.butler@clearscore.com>
2022-07-25 10:20:00 +0100

    Fix chakra styling and linting rules

--- packages/library/lib/components/button/index.js
+++ packages/library/lib/components/button/index.js
@@ -12,11 +12,12 @@ var _react2 = require("@chakra-ui/react");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function GenericButton(_ref) {
-  var colourScheme = _ref.colourScheme,
+  var _ref$colourScheme = _ref.colourScheme,
+      colourScheme = _ref$colourScheme === void 0 ? "teal" : _ref$colourScheme,
       disabled = _ref.disabled,
       children = _ref.children;
-  return /*#__PURE__*/_react["default"].createElement(_react2.Button, {
+  return /*#__PURE__*/_react["default"].createElement(_react2.ChakraProvider, null, /*#__PURE__*/_react["default"].createElement(_react2.Button, {
     colorScheme: colourScheme,
     disabled: disabled
-  }, children);
+  }, children));
 }
--- packages/library/src/components/button/index.jsx
+++ packages/library/src/components/button/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function GenericButton({ colourScheme, disabled, children }) {
-  return (
-    <Button colorScheme={colourScheme} disabled={disabled}>
-      {children}
-    </Button>
-  );
+export default function GenericButton({
+    colourScheme = "teal",
+    disabled,
+    children,
+}) {
+    return (
+        <ChakraProvider>
+            <Button colorScheme={colourScheme} disabled={disabled}>
+                {children}
+            </Button>
+        </ChakraProvider>
+    );
 }
